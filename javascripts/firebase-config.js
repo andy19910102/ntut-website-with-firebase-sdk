2308442f39e6313b86308419fe46321c8fa589d9
andy19910102 <andy19910102@gmail.com>
2021-12-03 10:39:13 +0800

    20211201

--- javascripts/firebase-config.js
+++ javascripts/firebase-config.js
@@ -1,1 +1,10 @@
 // Add your Firebase Project Config here...
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,32 @@
 // Scripts start here...
 
+// Get all the documents from productList collection
+db
+    .collection("productList")
+    .get()
+    // Successfully get the data
+    // .then(function(docList){})
+    .then(docList => {
+        // console.log("[docList]", docList);
+        // loop all the doc from docList
+        docList.forEach(doc => {
+            // console.log("[doc]", doc);
+            // Get the data object from the doc
+            const product = doc.data();
+            console.log("[product]", product);
+            const col = `<div>
+                <h3>${product.name}</h3>
+                <p>${product.price}</p>
+            </div>`;
+            // Select an Element from web page
+            // with an id="productList" 
+            $("#productList").append(col)
+        })
+    })
+    // If some error happened
+    // .catch( function(err){} )
+    .catch(err => {
+        console.log("[err]", err);
+    });
+
+
